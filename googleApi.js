0e855e67c01a969d50a4f92bb7263cc23365233d
Sim Neyzi <simneyzi@gmail.com>
2021-05-19 13:48:25 -0700

    remove keys

--- googleApi.js
+++ googleApi.js
@@ -2,16 +2,10 @@ const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
 require('dotenv').config({ path: './.env' });
 
-// const CLIENT_ID = '619387599818-c83lga0gkep69hseje689ipnsh482qh4.apps.googleusercontent.com';
-// const CLIENT_SECRET = '2LUxPaWKKF3ygzveDv5jGsJ6';
-// const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-// const REFRESH_TOKEN = '1//04V250JfG2PF9CgYIARAAGAQSNwF-L9IragnOjilylyYZCvGgzh7kNCuzw8t_iYRGMYUFD8lIZNGHOcYsd5vVyp2f4PjuB_H__WE';
-
-
-const {CLIENT_ID} = process.env
-const {CLIENT_SECRET} = process.env
-const {REDIRECT_URI} = process.env
-const {REFRESH_TOKEN} = process.env
+const {CLIENT_ID} = process.env;
+const {CLIENT_SECRET} = process.env;
+const {REDIRECT_URI} = process.env;
+const {REFRESH_TOKEN} = process.env;
 
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
